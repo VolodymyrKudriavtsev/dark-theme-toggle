c21e2ed3617630b3a9f968ca5e9416507c453384
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2023-01-07 01:45:04 +0200

    some changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const refs = {
-  toggle: document.querySelector(".switcher"),
+  switcher: document.querySelector(".switcher"),
   HTML: document.querySelector("html"),
 };
 
@@ -7,17 +7,17 @@ const addDarkClassToHTML = () => {
   try {
     if (localStorage.getItem("theme") === "dark") {
       refs.HTML.classList.add("dark");
-      refs.toggle.elements[1].checked = true;
+      refs.switcher.elements[1].checked = true;
     } else {
       refs.HTML.classList.remove("dark");
-      refs.toggle.elements[0].checked = true;
+      refs.switcher.elements[0].checked = true;
     }
   } catch (err) {
     return;
   }
 };
 
-refs.toggle.addEventListener("change", (e) => {
+refs.switcher.addEventListener("change", (e) => {
   if (e.target.value === "dark") {
     refs.HTML.classList.add("dark");
     localStorage.setItem("theme", "dark");
